2a548e47de094217ec4a8717bb6a52aa45582213
Bo Chen <bo.chen@westpac.com.au>
2018-07-13 12:18:59 +1000

    lodash/fp

--- src/packages/recompose/__tests__/mapProps-test.js
+++ src/packages/recompose/__tests__/mapProps-test.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
+import { omit } from 'lodash/fp';
 import { mapProps, withState, compose } from '../';
 
-test('mapProps maps owner props to child props', () => {
-  const component = sinon.spy(() => null);
-  component.displayName = 'component';
+describe('mapProps', () => {
+  it.only('shoudl compose with lodash', () => {
+    const component = sinon.spy(() => null);
+    component.displayName = 'component';
 
-  const StringConcat = compose(
-    withState('strings', 'updateStrings', ['do', 're', 'mi']),
-    mapProps(({ strings, ...rest }) => ({
-      ...rest,
-      string: strings.join(''),
-    }))
-  )(component);
+    const OmitPropsComponent = compose(
+      mapProps(props => {
+        console.log(props); // { prop1: 'prop1', prop2: 'prop2' }
+        const result = omit(['prop1'], props);
+        console.log(result); // { prop1: 'prop1' }
+        return result;
+      })
+    )(component);
 
-  expect(StringConcat.displayName).toBe('withState(mapProps(component))');
+    expect(OmitPropsComponent.displayName).toBe('mapProps(component)');
 
-  mount(<StringConcat />);
-  const props = component.firstCall.args[0];
-  const { updateStrings } = props;
+    mount(<OmitPropsComponent prop1="prop1" prop2="prop2" />);
+    const props = component.firstCall.args[0];
+    expect(props).toEqual({ prop2: 'prop2' });
+  });
 
-  updateStrings(strings => [...strings, 'fa']);
+  it('mapProps maps owner props to child props', () => {
+    const component = sinon.spy(() => null);
+    component.displayName = 'component';
 
-  expect(component.firstCall.args[0].string).toBe('doremi');
-  expect(component.secondCall.args[0].string).toBe('doremifa');
+    const StringConcat = compose(
+      withState('strings', 'updateStrings', ['do', 're', 'mi']),
+      mapProps(({ strings, ...rest }) => ({
+        ...rest,
+        string: strings.join(''),
+      }))
+    )(component);
+
+    expect(StringConcat.displayName).toBe('withState(mapProps(component))');
+
+    mount(<StringConcat />);
+    const props = component.firstCall.args[0];
+    const { updateStrings } = props;
+
+    updateStrings(strings => [...strings, 'fa']);
+
+    expect(component.firstCall.args[0].string).toBe('doremi');
+    expect(component.secondCall.args[0].string).toBe('doremifa');
+  });
 });
