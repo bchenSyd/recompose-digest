f40a69ae00f5c96af77634d8621f1856d62be2d7
Bo Chen <bo.chen@westpac.com.au>
2018-07-13 14:18:25 +1000

    mapProps and lodash/fp

--- src/packages/recompose/__tests__/mapProps-test.js
+++ src/packages/recompose/__tests__/mapProps-test.js
@@ -5,18 +5,46 @@ import { omit } from 'lodash/fp';
 import { mapProps, withState, compose } from '../';
 
 describe('mapProps', () => {
-  it.only('shoudl compose with lodash', () => {
+  it('shoudl compose with lodash basic form', () => {
     const component = sinon.spy(() => null);
     component.displayName = 'component';
 
-    const OmitPropsComponent = compose(
-      mapProps(props => {
-        console.log(props); // { prop1: 'prop1', prop2: 'prop2' }
-        const result = omit(['prop1'], props);
-        console.log(result); // { prop1: 'prop1' }
-        return result;
-      })
-    )(component);
+    const OmitPropsComponent = mapProps(props => {
+      console.log(props); // { prop1: 'prop1', prop2: 'prop2' }
+      const result = omit(['prop1'], props);
+      console.log(result); // { prop1: 'prop1' }
+      return result;
+    })(component);
+
+    expect(OmitPropsComponent.displayName).toBe('mapProps(component)');
+
+    mount(<OmitPropsComponent prop1="prop1" prop2="prop2" />);
+    const props = component.firstCall.args[0];
+    expect(props).toEqual({ prop2: 'prop2' });
+  });
+
+  it('shoudl compose with lodash advanced', () => {
+    const component = sinon.spy(() => null);
+    component.displayName = 'component';
+
+    const OmitPropsComponent = mapProps(omit(['prop1']))(component);
+
+    expect(OmitPropsComponent.displayName).toBe('mapProps(component)');
+
+    mount(<OmitPropsComponent prop1="prop1" prop2="prop2" />);
+    const props = component.firstCall.args[0];
+    expect(props).toEqual({ prop2: 'prop2' });
+  });
+
+  it('shoudl compose with lodash final: https://github.com/acdlite/recompose/blob/master/docs/API.md#mapprops', () => {
+    const component = sinon.spy(() => null);
+    component.displayName = 'component';
+
+    // curring: for omit to take in ['prop1'] as input and output a partially implemented function, 
+    // which will feed mapProps
+    // very clever!
+    // compose(function1, <- function2, <- function3)(para for **function 3**)
+    const OmitPropsComponent = compose(mapProps,omit)(['prop1'])(component);
 
     expect(OmitPropsComponent.displayName).toBe('mapProps(component)');
 
